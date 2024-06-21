acf07ad56624dbde2bcdfe3f5be8b531ca0b58bb
bAd <rosiluca2001@gmail.com>
2024-06-21 19:19:26 +0200

    Made character selection, added rules of the game and created new api route for dungeon levels

--- DungeonGame/src/app/components/game/game.component.ts
+++ DungeonGame/src/app/components/game/game.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, NgFor, RouterModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+  styleUrl: './game.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameComponent {
+  
+  characters: { name: string, description: string }[] = [
+    { "name": 'Barbarian', "description": 'Once per turn, you may choose to reroll all dice when on 1 Health' },
+    { "name": 'Cleric', "description": 'If you roll the same number with all Energy dice, you can increase it by 2 (max.6)' },
+    { "name": 'Knight', "description": 'Once per Dungeon Level you may assign 2 Energy dice of the same value' },
+    { "name": 'Necromancer', "description": 'Once per Dungeon Level you may choose to lose 1 HP to inflict 1 Damage' },
+    { "name": 'Paladin', "description": 'Once per Dungeon Level you may leave one Energy Dice in place from last turn' },
+    { "name": 'Ranger', "description": 'Once per Dungeon Level you may assign a die to Range instead of Speed' },
+    { "name": 'Rogue', "description": 'Once per Dungeon Level you may increase the value of all Energy dice rolled by 1' },
+    { "name": 'Wizard', "description": 'Once per Dungeon Level you may reroll all Energy dice' },
+  ];
 
+  chooseCharacter(characterName: string): string {
+    console.log(characterName);
+    return characterName;
+  }
 }
--- NodeJS Web Server/server.js
+++ NodeJS Web Server/server.js
@@ -3,10 +3,20 @@ const path = require('path');
 // Jsons for Api calls
 const {bosses} = require('./game/creatures/enemies/bosses');
 const {minions} = require('./game/creatures/enemies/minions');
+const {dungeon} = require('./game/dungeon');
 
 // Express module
 const express = require("express");
 const app = express();
+
+// Credentials for MySQL DB
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'sakila'
+})
 
 // Use static folder of the angular project app
 app.use(express.static(path.join(__dirname,'../DungeonGame/dist/dungeon-game/browser')));
@@ -17,20 +27,52 @@ app.get('/api/minions', (req, res) => {
 });
 app.get('/api/minions/:position', (req, res) => {
   const { position } = req.params;
-  res.json(minions[position]);
+  if(res.json(minions[position]) === undefined) {
+    res.send('<h1>Resource does not exists</h1>');
+  }
+  else {
+    res.json(minions[position]);
+  }
 });
 app.get('/api/bosses', (req, res) => {
   res.json(bosses);
 });
 app.get('/api/bosses/:position', (req, res) => {
   const { position } = req.params;
-  res.json(bosses[position]);
+  if(bosses[position] === undefined) {
+    res.send('<h1>Resource does not exists</h1>');
+  }
+  else {
+    res.json(bosses[position]);
+  }
+});
+app.get('/api/dungeon', (req, res) => {
+  res.json(dungeon);
+});
+app.get('/api/dungeon/:level', (req, res) => {
+  const { level } = req.params;
+  if(dungeon[level] === undefined) {
+    res.send('<h1>Resource does not exists</h1>');
+  }
+  else {
+    res.json(dungeon[level]);
+  }
 });
 
 // All paths at Angular App
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'../DungeonGame/dist/dungeon-game/browser/index.html'));
 });
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
 
 // Server's App is listening on port 3000
 app.listen(3000, () => {
