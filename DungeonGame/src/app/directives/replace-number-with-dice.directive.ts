e85d63b26a7f2f51985f1a8bada2ec81f72dadca
bAd <rosiluca2001@gmail.com>
2024-07-13 12:00:27 +0200

    Added pentagon shape for d12 die of bosses

--- DungeonGame/src/app/directives/replace-number-with-dice.directive.ts
+++ DungeonGame/src/app/directives/replace-number-with-dice.directive.ts
@@ -16,20 +16,25 @@ enum Dice {
 export class ReplaceNumberWithDiceDirective {
   constructor(private element: ElementRef) {}
   ngAfterViewInit() {
-    switch(this.element.nativeElement.innerHTML) {
-      case '1': this.element.nativeElement.innerHTML = Dice.One;
-        break;
-      case '2': this.element.nativeElement.innerHTML = Dice.Two;
-        break;
-      case '3': this.element.nativeElement.innerHTML = Dice.Three;
-        break;
-      case '4': this.element.nativeElement.innerHTML = Dice.Four;
-        break;
-      case '5': this.element.nativeElement.innerHTML = Dice.Five;
-        break;
-      case '6': this.element.nativeElement.innerHTML = Dice.Six;
-        break;
-      default: if(this.element.nativeElement.innerHTML != '') console.log("ReplaceNumberWithDiceDirective: The number of the element is not valid!");
+    if(this.element.nativeElement.innerHTML > 6 && this.element.nativeElement.innerHTML < 13) {
+      this.element.nativeElement.innerHTML = "<div style=\"clip-path: polygon( 50% 0, 100% 38%, 81% 100%, 19% 100%, 0 38%);background-color: red;\">" + this.element.nativeElement.innerHTML + "</div>"
+    }
+    else {
+      switch(this.element.nativeElement.innerHTML) {
+        case '1': this.element.nativeElement.innerHTML = Dice.One;
+          break;
+        case '2': this.element.nativeElement.innerHTML = Dice.Two;
+          break;
+        case '3': this.element.nativeElement.innerHTML = Dice.Three;
+          break;
+        case '4': this.element.nativeElement.innerHTML = Dice.Four;
+          break;
+        case '5': this.element.nativeElement.innerHTML = Dice.Five;
+          break;
+        case '6': this.element.nativeElement.innerHTML = Dice.Six;
+          break;
+        default: if(this.element.nativeElement.innerHTML != '') console.log("ReplaceNumberWithDiceDirective: The number of the element is not a die face!");
+      }
     }
   }
 }
